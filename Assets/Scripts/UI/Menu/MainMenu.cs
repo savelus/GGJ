b9b7af62d64c8c0b812fac6ffdd992ca100468fe
Кудрявцев Савелий <savelussava@mail.ru>
2024-01-30 20:02:57 +0500

    Add level types for more records

--- Assets/Scripts/UI/Menu/MainMenu.cs
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -1,4 +1,8 @@
-﻿using TMPro;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Levels;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -9,13 +13,23 @@ namespace UI.Menu
     {
         [SerializeField] private Button _startButton;
         [SerializeField] private TextMeshProUGUI _textRecord;
+        [SerializeField] private List<LabelWithLevelType> _records;
 
         public void SubscribeOnStartButton(UnityAction callback) {
             _startButton.onClick.AddListener(callback);
         }
 
-        public void SetRecord(int record) {
-            _textRecord.text = record.ToString();
+        public void SetRecord(LevelType levelType, int record) {
+            var textBlock = _records.FirstOrDefault(x => x.LevelType == levelType);
+            if(textBlock==null)return;
+            
+            textBlock.Label.text = record.ToString();
+        }
+
+        public void ViewAllRecords() {
+            foreach (var recordWithType in _records) {
+                recordWithType.Label.text = PlayerPrefs.GetInt(recordWithType.LevelType.ToString()).ToString();
+            }
         }
     }
 }
